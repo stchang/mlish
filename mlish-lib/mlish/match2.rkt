fb7b0dfa9a2b93e120c19fab91d91135077e18ba
Stephen Chang <stchang@ccs.neu.edu>
2020-02-18 10:24:41 -0500

    split match2 into separate library

---
+++ mlish-lib/mlish/match2.rkt
@@ -0,0 +1,207 @@
+#lang turnstile
+
+(provide match2)
+
+(require (only-in racket/match [match match-])
+         (only-in "mlish.rkt" tup cons nil list ~× ×? ~List List? ⊔))
+
+(begin-for-syntax
+  (define (get-ctx pat ty)
+    (unify-pat+ty (list pat ty)))
+  (define (unify-pat+ty pat+ty)
+    (syntax-parse pat+ty
+     [(pat ty) #:when (brace? #'pat) ; handle root pattern specially (to avoid some parens)
+      (syntax-parse #'pat
+        [{(~datum _)} #'()]
+        [{(~literal nil)}  #'()]
+        [{A:id} ; disambiguate 0-arity constructors (that don't need parens)
+         #:when (get-extra-info #'ty)
+         #'()]
+        ;; comma tup syntax always has parens
+        [{(~and ps (p1 (unq p) ...))}
+         #:when (not (stx-null? #'(p ...)))
+         #:when (andmap (lambda (u) (equal? u 'unquote)) (syntax->datum #'(unq ...)))
+         (unify-pat+ty #'(ps ty))]
+        [{p ...} 
+         (unify-pat+ty (list (syntax/loc #'pat (p ...)) #'ty))])] ; pair
+      [((~datum _) ty) #'()]
+      [((~or (~literal nil)) ty) #'()]
+      [(A:id ty) ; disambiguate 0-arity constructors (that don't need parens)
+       #:with (_ (_ (_ C) . _) ...) (get-extra-info #'ty)
+       #:when (member (syntax->datum #'A) (syntax->datum #'(C ...)))
+       #'()]
+      [(x:id ty)  #'((x ty))]
+      [((p1 (unq p) ...) ty) ; comma tup stx
+       #:when (not (stx-null? #'(p ...)))
+       #:when (andmap (lambda (u) (equal? u 'unquote)) (syntax->datum #'(unq ...)))
+       #:with (~× t ...) #'ty
+       #:with (pp ...) #'(p1 p ...)
+       (unifys #'([pp t] ...))]
+      [(((~literal tup) p ...) ty) ; tup
+       #:with (~× t ...) #'ty
+       (unifys #'([p t] ...))]
+      [(((~literal list) p ...) ty) ; known length list
+       #:with (~List t) #'ty
+       (unifys #'([p t] ...))]
+     [(((~seq p (~datum ::)) ... rst) ty) ; nicer cons stx
+      #:with (~List t) #'ty
+       (unifys #'([p t] ... [rst ty]))]
+      [(((~literal cons) p ps) ty) ; arb length list
+       #:with (~List t) #'ty
+       (unifys #'([p t] [ps ty]))]
+      [((Name:id p ...) ty)
+       #:do[(define extra-info (get-extra-info #'ty))]
+       #:when extra-info
+       #:with (_ (_ Cons) _ _ [_ _ τ] ...)
+              (stx-findf
+                (syntax-parser
+                 [(_ 'C . rst) (datum=? #'Name #'C)])
+                (stx-cdr extra-info))
+       (unifys #'([p τ] ...))]
+     [((p) t)
+       #:when (type-error #:src #'p
+                #:msg "Pattern ~a does not match type of scrutinee ~a. Try dropping outer parens?"
+                          #'(p) #'t)
+       #'()]
+     [(p t)
+       #:when (type-error #:src #'p
+               #:msg "Pattern ~a does not match type of scrutinee ~a."
+                          #'p #'t)
+       #'()]))
+  (define (unifys p+tys) (stx-appendmap unify-pat+ty p+tys))
+  
+  (define (compile-pat p ty)
+    (syntax-parse p
+     [pat #:when (brace? #'pat) ; handle root pattern specially (to avoid some parens)
+      (syntax-parse #'pat
+        [{(~datum _)} #'_]
+        [{(~literal nil)}  (syntax/loc p (list))]
+        [{A:id} ; disambiguate 0-arity constructors (that don't need parens)
+         #:when (get-extra-info ty)
+         (compile-pat #'(A) ty)]
+        ;; comma tup stx always has parens
+        [{(~and ps (p1 (unq p) ...))}
+         #:when (not (stx-null? #'(p ...)))
+         #:when (andmap (lambda (u) (equal? u 'unquote)) (syntax->datum #'(unq ...)))
+         (compile-pat #'ps ty)]
+        [{pat ...} (compile-pat (syntax/loc p (pat ...)) ty)])]
+     [(~datum _) #'_]
+     [(~literal nil) ; nil
+      #'(list)]
+     [A:id ; disambiguate 0-arity constructors (that don't need parens)
+      #:with (_ (_ (_ C) . _) ...) (get-extra-info ty)
+      #:when (member (syntax->datum #'A) (syntax->datum #'(C ...)))
+      (compile-pat #'(A) ty)]
+     [x:id p]
+     [(p1 (unq p) ...) ; comma tup stx
+      #:when (not (stx-null? #'(p ...)))
+      #:when (andmap (lambda (u) (equal? u 'unquote)) (syntax->datum #'(unq ...)))
+      #:with (~× t ...) ty
+      #:with (p- ...) (stx-map (lambda (p t) (compile-pat p t)) #'(p1 p ...) #'(t ...))
+      #'(list p- ...)]
+     [((~literal tup) . pats)
+      #:with (~× . tys) ty
+      #:with (p- ...) (stx-map (lambda (p t) (compile-pat p t)) #'pats #'tys)
+      (syntax/loc p (list p- ...))]
+     [((~literal list) . ps)
+      #:with (~List t) ty
+      #:with (p- ...) (stx-map (lambda (p) (compile-pat p #'t)) #'ps)
+      (syntax/loc p (list p- ...))]
+     [((~seq pat (~datum ::)) ... last) ; nicer cons stx
+      #:with (~List t) ty
+      #:with (p- ...) (stx-map (lambda (pp) (compile-pat pp #'t)) #'(pat ...))
+      #:with last- (compile-pat #'last ty)
+      (syntax/loc p (list-rest p- ... last-))]
+     [((~literal cons) p ps)
+      #:with (~List t) ty
+      #:with p- (compile-pat #'p #'t)
+      #:with ps- (compile-pat #'ps ty)
+      #'(cons p- ps-)]
+     [(Name:id . pats)
+      #:with (_ (_ Cons) (_ StructName) _ [_ _ τ] ...)
+             (stx-findf
+               (syntax-parser
+                [(_ 'C . rst) (datum=? #'Name #'C)])
+               (stx-cdr (get-extra-info ty)))
+      #:with (p- ...) (stx-map compile-pat #'pats #'(τ ...))
+      (syntax/loc p (StructName p- ...))]))
+
+  ;; pats = compiled pats = racket pats
+  (define (check-exhaust pats ty)
+    (define (else-pat? p)
+      (syntax-parse p [(~literal _) #t] [_ #f]))
+    (define (nil-pat? p)
+      (syntax-parse p
+        [((~literal list)) #t]
+        [_ #f]))
+    (define (non-nil-pat? p)
+      (syntax-parse p
+        [((~literal list-rest) . rst) #t]
+        [((~literal cons) . rst) #t]
+        [_ #f]))
+    (define (tup-pat? p)
+      (syntax-parse p
+        [((~literal list) . _) #t] [_ #f]))
+    (cond
+     [(or (stx-ormap else-pat? pats) (stx-ormap identifier? pats)) #t]
+     [(List? ty) ; lists
+      (unless (stx-ormap nil-pat? pats)
+        (error 'match (let ([last (car (stx-rev pats))])
+                         (format "(~a:~a) missing nil clause for list expression"
+                                 (syntax-line last) (syntax-column last)))))
+      (unless (stx-ormap non-nil-pat? pats)
+        (error 'match (let ([last (car (stx-rev pats))])
+                         (format "(~a:~a) missing clause for non-empty, arbitrary length list"
+                                 (syntax-line last) (syntax-column last)))))
+      #t]
+     [(×? ty) ; tuples
+      (unless (stx-ormap tup-pat? pats)
+        (error 'match (let ([last (car (stx-rev pats))])
+                         (format "(~a:~a) missing pattern for tuple expression"
+                                 (syntax-line last) (syntax-column last)))))
+      (syntax-parse pats
+        [((_ p ...) ...)
+         (syntax-parse ty
+           [(~× t ...)
+            (apply stx-andmap 
+                   (lambda (t . ps) (check-exhaust ps t)) 
+                   #'(t ...) 
+                   (syntax->list #'((p ...) ...)))])])]
+     [else ; algebraic datatypes
+      (syntax-parse (get-extra-info ty)
+        [(_ (_ (_ C) (_ Cstruct) . rst) ...)
+         (syntax-parse pats
+           [((Cpat _ ...) ...)
+            (define Cs (syntax->datum #'(C ...)))
+            (define Cstructs (syntax->datum #'(Cstruct ...)))
+            (define Cpats (syntax->datum #'(Cpat ...)))
+            (unless (set=? Cstructs Cpats)
+              (error 'match
+                (let ([last (car (stx-rev pats))])
+                  (format "(~a:~a) clauses not exhaustive; missing: ~a"
+                          (syntax-line last) (syntax-column last)
+                          (string-join      
+                            (for/list ([C Cs][Cstr Cstructs] #:unless (member Cstr Cpats))
+                              (symbol->string C))
+                            ", ")))))
+            #t])]
+        [_ #t])]))
+
+  ;; TODO: do get-ctx and compile-pat in one pass
+  (define (compile-pats pats ty)
+    (stx-map (lambda (p) (list (get-ctx p ty) (compile-pat p ty))) pats))
+  )
+
+(define-typed-syntax match2 #:datum-literals (with ->)
+  [(_ e with . clauses) ≫
+   #:fail-unless (not (null? (syntax->list #'clauses))) "no clauses"
+   [⊢ e ≫ e- ⇒ τ_e]
+   #:with ([(~seq p ...) -> e_body] ...) #'clauses
+   #:with (pat ...) (stx-map ; use brace to indicate root pattern
+                     (lambda (ps) (syntax-parse ps [(pp ...) (syntax/loc this-syntax {pp ...})]))
+                     #'((p ...) ...))
+   #:with ([(~and ctx ([x ty] ...)) pat-] ...) (compile-pats #'(pat ...) #'τ_e)
+   [[x ≫ x- : ty] ... ⊢ (pass-expected e_body #,this-syntax) ≫ e_body- ⇒ ty_body] ...
+   #:when (check-exhaust #'(pat- ...) #'τ_e)
+   --------
+   [⊢ (match- e- [pat- (let-values- ([(x-) x] ...) e_body-)] ...) ⇒ (⊔ ty_body ...)]])
